387217a60bca16c2c01e04f81c9bb727c3ab1059
Julienraptor01 <julienraptor01@gmail.com>
2024-07-15 15:50:41 +0200

    Inittial commit

---
+++ src/main/java/me/julienraptor01/Main.java
@@ -0,0 +1,10 @@
+package me.julienraptor01;
+
+import java.util.logging.Logger;
+
+public class Main {
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+	public static void main(String[] args) {
+	}
+}
---
+++ src/test/java/me/julienraptor01/MainTest.java
@@ -0,0 +1,12 @@
+package me.julienraptor01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+	@Test
+	public void Test() {
+		assertTrue(true);
+	}
+}
