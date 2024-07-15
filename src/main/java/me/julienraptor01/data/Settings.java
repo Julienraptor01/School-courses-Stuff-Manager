e6bfc79da2185b4ba941d0d1a9bbdc98112967cd
Julienraptor01 <julienraptor01@gmail.com>
2024-07-15 18:52:47 +0200

    add doc for the settings

--- src/main/java/me/julienraptor01/data/Settings.java
+++ src/main/java/me/julienraptor01/data/Settings.java
@@ -12,25 +12,51 @@ public class Settings {
 
 	private static Settings instance = null;
 
+	/**
+	 * Private constructor to prevent instantiation
+	 */
 	private Settings() {
 	}
 
+	/**
+	 * Get the instance of the Settings class or create a new one if it doesn't exist
+	 * @return the instance of the Settings class
+	 */
 	public static Settings getInstance() {
 		return instance == null ? instance = new Settings() : instance;
 	}
 
+	/**
+	 * If the startMaximized is null, {@link #DEFAULT_START_MAXIMIZED} is returned
+	 * @return weather the application should start maximized or not
+	 */
 	public boolean isStartMaximized() {
 		return startMaximized == null ? DEFAULT_START_MAXIMIZED : startMaximized;
 	}
 
+	/**
+	 * Set if the application should start maximized or not<br>
+	 * When set to null {@link #isStartMaximized()} returns {@link #DEFAULT_START_MAXIMIZED}
+	 * @param startMaximized if the application should start maximized or not
+	 */
 	public void setStartMaximized(Boolean startMaximized) {
 		Settings.startMaximized = startMaximized;
 	}
 
+	/**
+	 * If the datetimeFormat is null, {@link #DEFAULT_DATETIME_FORMAT} is returned
+	 * @return the format of the date and time
+	 */
 	public String getDatetimeFormat() {
 		return datetimeFormat == null ? DEFAULT_DATETIME_FORMAT : datetimeFormat;
 	}
 
+	/**
+	 * Set the format of the date and time<br>
+	 * See {@link java.time.format.DateTimeFormatter} for the format syntax<br>
+	 * When set to null {@link #getDatetimeFormat()} returns {@link #DEFAULT_DATETIME_FORMAT}
+	 * @param datetimeFormat the format of the date and time
+	 */
 	public void setDatetimeFormat(String datetimeFormat) {
 		Settings.datetimeFormat = datetimeFormat;
 	}
