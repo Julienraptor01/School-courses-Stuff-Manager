6b9f1f731969cd27477f92b5ff155db14111b07c
Julienraptor01 <julienraptor01@gmail.com>
2024-07-15 18:37:09 +0200

    Create the Settings test

---
+++ src/test/java/me/julienraptor01/data/SettingsTest.java
@@ -0,0 +1,35 @@
+package me.julienraptor01.data;
+
+import org.junit.jupiter.api.Test;
+
+import static me.julienraptor01.data.Settings.DEFAULT_START_MAXIMIZED;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SettingsTest {
+	@Test
+	void getInstance() {
+		assertNotNull(Settings.getInstance());
+		assertEquals(Settings.getInstance(), Settings.getInstance());
+	}
+
+	@Test
+	void isStartMaximized() {
+		assertEquals(DEFAULT_START_MAXIMIZED, Settings.getInstance().isStartMaximized());
+		Settings.getInstance().setStartMaximized(false);
+		assertFalse(Settings.getInstance().isStartMaximized());
+		Settings.getInstance().setStartMaximized(true);
+		assertTrue(Settings.getInstance().isStartMaximized());
+		Settings.getInstance().setStartMaximized(null);
+		assertEquals(DEFAULT_START_MAXIMIZED, Settings.getInstance().isStartMaximized());
+	}
+
+	@Test
+	void getDatetimeFormat() {
+		assertEquals(Settings.DEFAULT_DATETIME_FORMAT, Settings.getInstance().getDatetimeFormat());
+		String datetimeFormat = "dd/MM/YYYY HH:mm:ss";
+		Settings.getInstance().setDatetimeFormat(datetimeFormat);
+		assertEquals(datetimeFormat, Settings.getInstance().getDatetimeFormat());
+		Settings.getInstance().setDatetimeFormat(null);
+		assertEquals(Settings.DEFAULT_DATETIME_FORMAT, Settings.getInstance().getDatetimeFormat());
+	}
+}
