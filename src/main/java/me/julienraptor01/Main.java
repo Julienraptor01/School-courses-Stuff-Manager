8141772e63d6ff178b7a73e03c44a1b412a1d002
Julienraptor01 <julienraptor01@gmail.com>
2024-07-19 15:41:29 +0200

    fix container implementation and test it

--- src/main/java/me/julienraptor01/Main.java
+++ src/main/java/me/julienraptor01/Main.java
@@ -1,11 +1,12 @@
 package me.julienraptor01;
 
-import me.julienraptor01.data.Container;
+import me.julienraptor01.data.*;
 import me.julienraptor01.data.config.Config;
 import me.julienraptor01.data.config.Settings;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Main {
@@ -20,6 +21,30 @@ public class Main {
 		settings.setStartMaximized(Settings.DEFAULT_START_MAXIMIZED);
 		settings.setDatetimeFormat(Settings.DEFAULT_DATETIME_FORMAT);
 		Config.saveConfig();
+		LOGGER.info(container.toString());
+		container.elements.add(new Bonus.Builder().name("Bonus").identifier("bonus").value(0.0).build());
+		LOGGER.info(container.toString());
+		container.elements.add(new Component.Builder().name("Component").identifier("component").rarity(Rarity.ADMIN).build());
+		LOGGER.info(container.toString());
+		container.elements.add(new Item.Builder().name("Item").identifier("item").rarity(Rarity.ADMIN).reforge("reforge").build());
+		LOGGER.info(container.toString());
+		container.elements.add(new Pet.Builder().name("Pet").identifier("pet").rarity(Rarity.ADMIN).petItem((Item) container.elements.stream().filter(element -> element instanceof Item).findFirst().orElse(null)).build());
+		LOGGER.info(container.toString());
+		container.elements.stream().filter(element -> element instanceof Item).findFirst().ifPresent(element -> container.elements.remove(element));
+		LOGGER.info(container.toString());
+		container.elements.remove(1);
+		LOGGER.info(container.toString());
+		container.elements.stream().filter(element -> element instanceof Pet).findFirst().ifPresent(element -> container.elements.remove(element));
+		LOGGER.info(container.toString());
+		container.elements.removeFirst();
+		LOGGER.info(container.toString());
+		container.elements.add(new Bonus.Builder().name("Bonus").identifier("bonus").value(0.0).build());
+		LOGGER.info(container.toString());
+		container.elements.removeLast();
+		LOGGER.info(container.toString());
+		container.elements.addAll(List.of(new Item.Builder().name("Item").identifier("item").rarity(Rarity.ADMIN).reforge("reforge").build(), new Pet.Builder().name("Pet").identifier("pet").rarity(Rarity.ADMIN).build()));
+		LOGGER.info(container.toString());
+		container.elements.clear();
 		LOGGER.info(container.toString());
 	}
 }
--- src/main/java/me/julienraptor01/data/Container.java
+++ src/main/java/me/julienraptor01/data/Container.java
@@ -1,24 +1,20 @@
 package me.julienraptor01.data;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /**
  * This singleton class is used to store the elements of the application.
  */
 public class Container {
-	private static final Logger LOGGER = Logger.getLogger(Container.class.getName());
-
-	private static List<BasicElement> elements = List.of();
-
 	private static Container instance = null;
+	public List<BasicElement> elements = new LinkedList<>();
 
 	/**
 	 * Private constructor to prevent instantiation
 	 */
 	private Container() {
-		elements = List.of(new Bonus.Builder().name("Bonus").identifier("bonus").value(0.0).build(), new Component.Builder().name("Component").identifier("component").rarity(Rarity.ADMIN).build(), new Item.Builder().name("Item").identifier("item").rarity(Rarity.ADMIN).reforge("reforge").build(), new Pet.Builder().name("Pet").identifier("pet").rarity(Rarity.ADMIN).petItem(new Item.Builder().build()).build());
 	}
 
 	/**
@@ -28,15 +24,6 @@ public class Container {
 	 */
 	public static Container getInstance() {
 		return instance == null ? instance = new Container() : instance;
-	}
-
-	/**
-	 * Get the elements of the application
-	 *
-	 * @return the elements of the application
-	 */
-	public List<BasicElement> getElements() {
-		return elements;
 	}
 
 	@Override
